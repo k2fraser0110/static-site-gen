8ab8482bc1eec55c25cbfa801c69b5e51e0cd1b1
k2fraser0110 <k2fraser@gmail.com>
2026-04-14 17:40:32 -0400

    test page on github

--- src/main.py
+++ src/main.py
@@ -4,10 +4,14 @@ from htmlnode import HTMLNode, ParentNode, LeafNode
 from converter import markdown_to_html_node
 import os
 import shutil
+import sys
 
 def main():
-    directory_copy("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = "/"
+    if sys.argv[0] != "":
+        basepath = sys.argv[0]
+    directory_copy("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 def directory_copy(src, dest):
     source_path = f"{os.getcwd()}/{src}/"
@@ -28,7 +32,7 @@ def extract_title(markdown):
         if line[:2] == "# ": return line[2:]
     raise Exception("No header line")
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     print(dir_path_content, dest_dir_path)
     full_dir_path = f"{os.getcwd()}/{dir_path_content}/"
     full_dest_path = f"{os.getcwd()}/{dest_dir_path}/"
@@ -50,13 +54,15 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 
             template = template.replace("{{ Title }}", title)
             template = template.replace("{{ Content }}", html)
+            template = template.replace('href="/', f'href="{basepath}')
+            template = template.replace('src="/', f'src="{basepath}')
 
             with open(full_dest_path + file_name[:len(file_name)-3] + ".html", "w") as f:
                 f.write(template)
             f.close()
         elif not os.path.isfile(full_dir_path + file_name):
             if not os.path.exists(full_dest_path + file_name): os.mkdir(full_dest_path + file_name)
-            generate_pages_recursive(f"{dir_path_content}/{file_name}", template_path, f"{dest_dir_path}/{file_name}")
+            generate_pages_recursive(f"{dir_path_content}/{file_name}", template_path, f"{dest_dir_path}/{file_name}", basepath)
 
 def generate_page(from_path, template_path, dest_path):
     full_from_path = f"{os.getcwd()}/{from_path}"
